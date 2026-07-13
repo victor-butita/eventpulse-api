5a7255aac4a4d1741d9b8246851c3e9b23fad8f7
Oduor-Antony <anthonyoduor99@gmail.com>
2026-07-13 08:25:09 -0400

    fix: update user entity and Liquibase migration

--- src/main/kotlin/com/eventpulse/api/entity/User.kt
+++ src/main/kotlin/com/eventpulse/api/entity/User.kt
@@ -18,9 +18,6 @@ class User (
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
 
-    @Column(nullable = false)
-    var name: String,
-
     @Column(nullable = false, unique = true)
     var email: String,
 
